6bc367ec3434f6918919e11de081b107b910214b
billfres <ngomafresnel@gmail.com>
2022-03-24 00:18:47 +0100

    Last commit

--- src/main/java/com/fresnel/springsecurityjpa/models/MyUserDetails.java
+++ src/main/java/com/fresnel/springsecurityjpa/models/MyUserDetails.java
@@ -7,31 +7,28 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MyUserDetails implements UserDetails {
     private String userName;
     private String password;
     private boolean active;
-    //private List<GrantedAuthority> authorities;
+    private List<GrantedAuthority> authorities;
 
     public MyUserDetails(){}
 
-    public MyUserDetails(String userName){
-        this.userName = userName;
-    }
-
-    /*public MyUserDetails(User user) {
+    public MyUserDetails(User user) {
         this.userName = user.getUserName();
         this.password = user.getPassword();
         this.active = user.isActive();
         this.authorities = Arrays.stream(user.getRoles().split(","))
                 .map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
-    }*/
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Arrays.asList(new SimpleGrantedAuthority("USER_ROLE"));
+        return authorities;
     }
 
     @Override
--- src/main/java/com/fresnel/springsecurityjpa/service/MyUserDetailsService.java
+++ src/main/java/com/fresnel/springsecurityjpa/service/MyUserDetailsService.java
@@ -1,15 +1,27 @@
 package com.fresnel.springsecurityjpa.service;
 
 import com.fresnel.springsecurityjpa.models.MyUserDetails;
+import com.fresnel.springsecurityjpa.models.User;
+import com.fresnel.springsecurityjpa.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return new MyUserDetails(s);
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUserName(username);
+
+        user.orElseThrow(()-> new UsernameNotFoundException("User Not Found: " + username));
+        return  user.map(MyUserDetails::new).get();
     }
 }
